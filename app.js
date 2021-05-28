934a71fe905853c51ae88066ef479233ef71791d
Sainath Rao <ssnr_pathange@ece.iitr.ac.in>
2021-05-28 13:06:08 +0530

    only one user data allowed || UPDATED

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var checkoutRouter = require('./routes/checkout');
 var checkinRouter = require('./routes/checkin');
 var approvedbooksRouter = require('./routes/approvedbooks');
 var managebooksRouter = require('./routes/managebooks');
+var checkedbooksRouter = require('./routes/checkedbooks');
 var managedataRouter = require('./routes/managedata');
 var managedata0Router = require('./routes/managedata0');
 var userhistoryRouter = require('./routes/userhistory');
@@ -33,6 +34,7 @@ app.post('/checkout',checkoutRouter);
 app.post('/managedata',managedataRouter);
 app.post('/managedata0',managedata0Router);
 app.get('/managebooks',managebooksRouter);
+app.get('/checkedbooks',checkedbooksRouter);
 app.get('/books',booksRouter);
 app.get('/approvedbooks',approvedbooksRouter);  
 app.get('/userhistory',userhistoryRouter);
@@ -128,19 +130,23 @@ app.post('/addbooks',(req,res)=>{
 app.get('/login',(req,res)=>{
    res.render('login.ejs')
 })
+app.get('/loginE',(req,res)=>{
+    res.render('loginE.ejs')
+ })
 
 app.post('/login', async (req,res)=>{
     const email = req.body.email;
     const password = req.body.password;
     // const hashedPassword =await bcrypt.hash(req.body.password,10); 
     // const password = hashedPassword;
+    
     var sql = "SELECT * FROM USERS_DATA WHERE Email='"+email+"'";
     //console.log(sql);
     con.query(sql, (error, result, fields) => {
         if (error) {
           return console.error(error.message);
         }
-
+        
         if(result.length>0){
 
         Object.keys(result).forEach(function(key) { 
@@ -156,9 +162,11 @@ app.post('/login', async (req,res)=>{
                     res.redirect('/admin');
                 }
                 else{
+                    
                     sess = req.session;
                     sess.email = req.body.email;
                     res.redirect('/userLandPage');
+                    next();
                 }
             }
             else{
@@ -179,6 +187,53 @@ app.post('/login', async (req,res)=>{
     });
 })
 
+app.post('/loginexists', async (req,res)=>{
+    
+    const email = req.body.email;
+    var password = req.body.password;
+    const hashedPassword =await bcrypt.hash(req.body.password,10); 
+    
+
+    console.log(email);
+    console.log(password);
+    console.log("checking password");
+    con.query('SELECT * FROM USERS_DATA WHERE Email=?',[email], (error, result) => {
+
+       
+        var row;
+
+        Object.keys(result).forEach(function(key) { 
+        row = result[key];
+        });
+
+        console.log(row.Password);
+
+             if(bcrypt.compareSync(password, row.Password)){
+                console.log("Password Matches");
+                 if((result.Email=="admin@gmail.com")){
+                     console.log("admin");
+                     sess = req.session;
+                     sess.email = req.body.email;
+                     res.redirect('/admin');
+                     res.end();
+
+                 }
+                 else{
+                   
+                     sess = req.session;
+                     sess.email = req.body.email;
+                     res.redirect('/userLandPage');
+                     res.end();
+                 }
+            }
+            else{
+                res.send("Incorrect password");
+            }
+    
+});
+
+});
+
 
 
 app.get('/register', (req,res)=>{
@@ -189,21 +244,33 @@ app.post('/register',async(req,res)=>{
     try {
         const hashedPassword =await bcrypt.hash(req.body.password,10);  
         const password = hashedPassword;
-        
         // const password = req.body.password;
-
         const name = req.body.name;
         const email = req.body.email;
         
-        if(name && email && password){
-            var sql = "INSERT INTO USERS_DATA (Name,Email,Password) VALUES ('"+name+"', '"+email+"', '"+password+"')";  
-            con.query(sql, function (err, result) {     
-            if (err) throw err;  
-            console.log("USER DATA INSERTED");  
-            }); 
-            // console.log("Created User! Name: "+name+"Email:" +email+"Password: "+password);
-        }
-        res.redirect('/login');
+        con.query('SELECT * FROM USERS_DATA WHERE Email=?',[email], function (err, result,fields){
+            if(result.length>0){
+                //user already exists;
+                //redirect to some other page
+                console.log("user email already exits");
+                res.redirect('/loginE');
+                
+            }
+
+            else if(name && email && password){
+                var sql = "INSERT INTO USERS_DATA (Name,Email,Password) VALUES ('"+name+"', '"+email+"', '"+password+"')";  
+                con.query(sql, function (err, result) {     
+                if (err) throw err;  
+                console.log("USER DATA INSERTED");  
+                }); 
+                // console.log("Created User! Name: "+name+"Email:" +email+"Password: "+password);
+                res.redirect('/login');
+            }
+            
+
+        });
+
+
     } 
     catch {
         res.redirect('/register');
@@ -213,17 +280,3 @@ app.post('/register',async(req,res)=>{
 
 
 app.listen(3000);
-
-
-module.exports.getEmail = function(){
-    // return sessEmail;
-};
-
-
-
-
-
-
-
-//back button log outs . ! how to stop this
-
